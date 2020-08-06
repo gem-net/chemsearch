3b11d45743ba5ca4115a0937c81cadd33e24fdf1
Stephen Gaffney <stephen.gaffney@yale.edu>
2020-08-06 16:15:23 +0100

    Store local molecule collection as db global.

--- src/chemsearch/db.py
+++ src/chemsearch/db.py
@@ -7,17 +7,15 @@ from rdkit.DataStructs import TanimotoSimilarity
 from .models import LocalMolecule, Molecule
 
 
-LOCAL_DB_PATH = os.environ.get('LOCAL_DB_PATH')
-
-
 class MolException(Exception):
     pass
 
 
-def get_molecules(load_rdkit_mol=True):
+def load_molecules(load_rdkit_mol=True):
     # drive_path = os.path.join(LOCAL_DB_PATH, 'gdrive.tsv')
     # gd = pd.read_csv(drive_path, sep='\t')
-    summary_path = os.path.join(LOCAL_DB_PATH, 'summary.tsv')
+    local_db_path = os.environ.get('LOCAL_DB_PATH', 'local_db')
+    summary_path = os.path.join(local_db_path, 'summary.tsv')
     df = pd.read_csv(summary_path, sep='\t')
     # categories = sorted(list(df.category.unique()))
     # category_counts = df.category.value_counts().to_dict()
@@ -25,8 +23,14 @@ def get_molecules(load_rdkit_mol=True):
         yield LocalMolecule(rec, store_mol=load_rdkit_mol)
 
 
+def reload_molecules():
+    global LOCAL_MOLECULES
+    LOCAL_MOLECULES.clear()
+    LOCAL_MOLECULES.extend(load_molecules(load_rdkit_mol=True))
+
+
 def get_single_molecule():
-    gen = get_molecules()
+    gen = load_molecules()
     mol = next(gen)
     return mol
 
@@ -36,7 +40,7 @@ def get_substructure_matches(smiles):
     query = MolFromSmiles(smiles)
     if query is None:
         raise MolException("Error building molecule from input.")
-    matches = [i for i in get_molecules() if i.mol.HasSubstructMatch(query)]
+    matches = [i for i in LOCAL_MOLECULES if i.mol.HasSubstructMatch(query)]
     return matches
 
 
@@ -46,7 +50,7 @@ def get_sim_matches(smiles):
         raise MolException("Error building molecule from input.")
     fp_q = Molecule.get_morgan_fingerprint(query)
     results = []
-    for m in get_molecules(load_rdkit_mol=True):
+    for m in LOCAL_MOLECULES:
         fp = m.fingerprint_similarity_raw
         sim = TanimotoSimilarity(fp_q, fp)
         results.append((sim, m))
@@ -54,3 +58,6 @@ def get_sim_matches(smiles):
     results.sort(key=lambda t: t[0], reverse=True)
     sims, molecules = zip(*results)
     return sims, molecules
+
+
+LOCAL_MOLECULES = list(load_molecules(load_rdkit_mol=True))
