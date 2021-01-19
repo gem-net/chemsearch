b8f53ae77d93bf4a647f13ebbf1c7f416373ac95
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-01-19 17:25:23 +0000

    Simplify LocalMolecule init, add _Name to mol.

--- src/chemsearch/molecule.py
+++ src/chemsearch/molecule.py
@@ -67,7 +67,7 @@ class LocalMolecule(Molecule):
 
     def __init__(self, record: pd.Series, from_summary=True, store_mol=True):
         """Initialize from scanned molfile table or summary table record."""
-        if from_summary:
+        if from_summary:  # all metadata fields in record, via REFERENCE_PATH
             for field in self.fields_all:  # populate metadata
                 self.__setattr__(field, record[field])
             self.mol_path = self._get_mol_path()
@@ -75,23 +75,25 @@ class LocalMolecule(Molecule):
                 self.mol = Chem.MolFromMolFile(self.mol_path)
                 if self.mol is not None:
                     self.fingerprint_similarity_raw = Molecule.get_morgan_fingerprint(self.mol)
+                    self.mol.SetProp("_Name", self.mol_name)
                 else:
                     self.fingerprint_similarity_raw = None
             return
-        # otherwise from google drive molfile table
-        self.mol_path = self._get_mol_path_from_record(record)
+        # otherwise record is from SCAN_RESULTS
+        self.mol_path = self._get_mol_path_from_scan_record(record)
         m = Chem.MolFromMolFile(self.mol_path)
         super().__init__(m)
-        if not from_summary:  # add metadata
-            self.mol_id = record['id']  # MOL file ID in
-            self.mol_name = record['folder_name']
-            self.mol_filename = record['name']
-            self.folder_id = record['folder_id']
-            self.category = record['category']
-            self.user = record['lastModifyingUser']
-            self.mod_time = record['modifiedTime']
-            self.mol_basename = os.path.splitext(self.mol_filename)[0]
-            self.dir_url = f"https://drive.google.com/drive/u/0/folders/{self.folder_id}"
+        self.mol_id = record['id']  # MOL file ID in
+        self.mol_name = record['folder_name']
+        self.mol_filename = record['name']
+        self.folder_id = record['folder_id']
+        self.category = record['category']
+        self.user = record['lastModifyingUser']
+        self.mod_time = record['modifiedTime']
+        self.mol_basename = os.path.splitext(self.mol_filename)[0]
+        self.dir_url = f"https://drive.google.com/drive/u/0/folders/{self.folder_id}"
+        if self.is_valid:
+            self.mol.SetProp("_Name", self.mol_name)
 
     @property
     def url_svg(self):
@@ -119,8 +121,8 @@ class LocalMolecule(Molecule):
         return os.path.join(self.local_mol_dir, svg_basename)
 
     @staticmethod
-    def _get_mol_path_from_record(record=None):
-        """Get mol path from Drive table record."""
+    def _get_mol_path_from_scan_record(record=None):
+        """Get mol path from SCAN_RESULTS record (Drive or local)."""
         mol_dir = os.path.join(paths.ARCHIVE_DIR, record.category,
                                record.folder_name)
         mol_path = os.path.join(mol_dir, record['name'])
