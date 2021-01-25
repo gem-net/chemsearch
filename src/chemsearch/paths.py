bb912629d3f05174a253d67766a70fa745652e1d
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-01-25 10:10:46 +0000

    Add PATH_SOURCES for clearer versioning.

--- src/chemsearch/paths.py
+++ src/chemsearch/paths.py
@@ -1,21 +1,31 @@
 import os
 import pathlib
-
-ARCHIVE_DIR = os.environ.get('LOCAL_DB_PATH')
-CONFIG_DIR = str(pathlib.Path(__file__).parent.parent.parent\
-                 .joinpath('config').absolute())
-ENV_PATH = os.path.join(CONFIG_DIR, '.env')
-SCAN_RESULTS_PATH = os.path.join(ARCHIVE_DIR, 'scan_local.tsv')
-REFERENCE_PATH = os.path.join(ARCHIVE_DIR, 'master_local.tsv')
-SERVICE_ACCOUNT_CREDS = os.path.join(CONFIG_DIR, 'creds.json')
+from collections import defaultdict
 
 
 def update_paths(use_drive=False):
-    global ARCHIVE_DIR, SCAN_RESULTS_PATH, REFERENCE_PATH
+    global ARCHIVE_DIR, PATH_SOURCES, SCAN_RESULTS_PATH, REFERENCE_PATH
+    _version = 'gdrive' if use_drive else 'local'
     ARCHIVE_DIR = os.environ.get('LOCAL_DB_PATH')
-    if use_drive:
-        SCAN_RESULTS_PATH = os.path.join(ARCHIVE_DIR, 'scan_gdrive.tsv')
-        REFERENCE_PATH = os.path.join(ARCHIVE_DIR, 'master_gdrive.tsv')
-    else:
-        SCAN_RESULTS_PATH = os.path.join(ARCHIVE_DIR, 'scan_local.tsv')
-        REFERENCE_PATH = os.path.join(ARCHIVE_DIR, 'master_local.tsv')
+    PATH_SOURCES = _build_path_sources_dict(ARCHIVE_DIR)
+    SCAN_RESULTS_PATH = PATH_SOURCES[_version]['scan']
+    REFERENCE_PATH = PATH_SOURCES[_version]['master']
+
+
+def _build_path_sources_dict(db_dir):
+    sources = defaultdict(dict)
+    for version in ['local', 'gdrive']:
+        for source in ['scan', 'master']:
+            sources[version][source] = os.path.join(db_dir,
+                                                    f'{source}_{version}.tsv')
+    return sources
+
+
+ARCHIVE_DIR = os.environ.get('LOCAL_DB_PATH')
+CONFIG_DIR = str(pathlib.Path(__file__).parent.parent.parent
+                 .joinpath('config').absolute())
+ENV_PATH = os.path.join(CONFIG_DIR, '.env')
+SERVICE_ACCOUNT_CREDS = os.path.join(CONFIG_DIR, 'creds.json')
+PATH_SOURCES = None
+SCAN_RESULTS_PATH = None
+REFERENCE_PATH = None
