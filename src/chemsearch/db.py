27285f486ba9a7c2418986a06abc6f7f188feefc
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-01-25 10:27:49 +0000

    Add DuplicateTracker to track InChIKey dups.

--- src/chemsearch/db.py
+++ src/chemsearch/db.py
@@ -1,12 +1,13 @@
 import os
 import logging
+from collections import Counter, defaultdict, namedtuple
 
 import pandas as pd
 from rdkit.Chem import MolFromSmiles, MolFromSmarts
 from rdkit.DataStructs import TanimotoSimilarity
 
 from . import paths
-from .molecule import LocalMolecule, Molecule
+from .molecule import LocalMolecule, Molecule, MolFileNotFoundError
 
 
 class MolException(Exception):
@@ -34,13 +35,14 @@ def load_molecules(load_rdkit_mol=True):
 
 
 def reload_molecules():
-    global LOCAL_MOLECULES, MOLECULE_DICT
+    global LOCAL_MOLECULES, MOLECULE_DICT, DUPLICATE_TRACKER
     new_molecules = list(load_molecules(load_rdkit_mol=True))
     new_id_dict = {i.inchi_key: i for i in new_molecules}
     LOCAL_MOLECULES.clear()
     LOCAL_MOLECULES.extend(new_molecules)
     MOLECULE_DICT.clear()
     MOLECULE_DICT.update(new_id_dict)
+    DUPLICATE_TRACKER.update()
 
 
 def _load_reference_file_as_df():
@@ -76,5 +78,79 @@ def get_sim_matches(smiles, mols=None):
     return sims, molecules
 
 
-LOCAL_MOLECULES = list(load_molecules(load_rdkit_mol=True))
+class DuplicateTracker:
+
+    MolId = namedtuple('MolId', ['lm_ind', 'category', 'mol_name'])
+
+    def __init__(self, mol_list=None):
+        self.has_duplicates = False
+        self.duplicated_inchi_keys = set()
+        self.n_duplicates = 0
+        self.inchi_key_to_ids = dict()
+        self.inchi_key_to_inds = dict()
+        self.inchi_key_to_mols = dict()
+        self.ind_to_inds = dict()
+        self.ind_to_mols = dict()
+        self.names_to_inds = dict()
+        self.names_to_mols = dict()
+        self.update(mol_list)
+
+    def update(self, mol_list=None):
+        if mol_list is None:
+            mol_list = LOCAL_MOLECULES
+        else:
+            mol_list = list(mol_list)  # list required for indexing
+        if not mol_list:
+            return
+        counts = Counter([i.inchi_key for i in mol_list])
+        for inchi_key, count in counts.most_common():
+            if count > 1:
+                self.duplicated_inchi_keys.add(inchi_key)
+            else:
+                break
+        if self.duplicated_inchi_keys:
+            self.has_duplicates = True
+        self.n_duplicates = len(self.duplicated_inchi_keys)
+        repeats = defaultdict(set)
+        for ind, i in enumerate(mol_list):
+            if i.inchi_key in self.duplicated_inchi_keys:
+                repeats[i.inchi_key].add(DuplicateTracker.MolId(
+                    ind, i.category, i.mol_name))
+        for inchi_key in self.duplicated_inchi_keys:
+            id_set = repeats[inchi_key]
+            inds = sorted([i.lm_ind for i in id_set])
+            self.inchi_key_to_ids[inchi_key] = id_set
+            self.inchi_key_to_inds[inchi_key] = inds
+            self.inchi_key_to_mols[inchi_key] = [mol_list[i] for i in inds]
+            for mol_id in id_set:
+                matched_ids = id_set.difference((mol_id,))
+                ind = mol_id.lm_ind
+                matched_inds = sorted([i.lm_ind for i in matched_ids])
+                matched_mols = [mol_list[i] for i in matched_inds]
+                self.ind_to_inds[ind] = matched_inds
+                self.ind_to_mols[ind] = matched_mols
+                names = (mol_id.category, mol_id.mol_name)
+                self.names_to_inds[names] = matched_inds
+                self.names_to_mols[names] = matched_mols
+        if self.has_duplicates:
+            self.print_duplicates()
+
+    def print_duplicates(self):
+        if self.has_duplicates:
+            _logger.warning("DUPLICATE INCHIKEYS FOUND!")
+            for inchi_key in self.duplicated_inchi_keys:
+                ids = self.inchi_key_to_ids[inchi_key]
+                names = [(i.category, i.mol_name) for i in ids]
+                key_str = f"{inchi_key} duplicated in: " + ', '.join([str(i) for i in names])
+                _logger.info(key_str)
+        else:
+            _logger.info("No duplicates found.")
+
+
+try:
+    LOCAL_MOLECULES = list(load_molecules(load_rdkit_mol=True))
+except MolFileNotFoundError as e:
+    _logger.info(f"Aborting molecule load. {e}")
+    LOCAL_MOLECULES = []
 MOLECULE_DICT = {i.inchi_key: i for i in LOCAL_MOLECULES}
+DUPLICATE_TRACKER = DuplicateTracker()
