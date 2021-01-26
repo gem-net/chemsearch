a852e30a8bbd212688563226c3504893146db663
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-01-26 14:30:23 +0000

    Run rebuild in prelaunch script.

--- src/chemsearch/prelaunch.py
+++ src/chemsearch/prelaunch.py
@@ -5,7 +5,7 @@ import os
 
 from .app import db, Flask
 from .app.config import config
-from .paths import ENV_PATH
+from .paths import ENV_PATH, update_paths
 
 # ENSURE DOTENV VARIABLES HAVE LOADED (for gunicorn)
 from dotenv import load_dotenv
@@ -16,9 +16,12 @@ app = Flask(__name__)
 app.config.from_object(config[config_name])
 db.init_app(app)
 
-from .app.models import User, Rebuild
+from .app.models import User, Rebuild, ReferenceHash
 
 with app.app_context():
     db.create_all()
+    update_paths(app.config['USE_DRIVE'])
+    from .app import rebuild
+    rebuild.run_full_scan_and_rebuild(user=None, run_async=False)
 
 print("DB create_all complete.")
